769e85ff63f6a16d6472a8566dbb07544b49c754
witekosz <witekosz@gmail.com>
2019-11-06 16:37:50 +0100

    Engine change

--- app/db.py
+++ app/db.py
@@ -6,22 +6,25 @@ import settings
 from api.models import shelter, pet
 
 
-db_engine = create_engine(
+def get_engine():
+    engine = create_engine(
         database=settings.POSTGRES_DB,
         user=settings.POSTGRES_USER,
         password=settings.POSTGRES_PASSWORD,
         host=settings.POSTGRES_HOST,
         port=settings.POSTGRES_PORT,
-)
+        isolation_level='AUTOCOMMIT'
+    )
+    return engine
 
 
-def create_tables(engine):
+def create_tables():
     meta = sa.MetaData()
-    meta.create_all(bind=engine, tables=[shelter, pet])
+    meta.create_all(bind=get_engine(), tables=[shelter, pet])
 
 
-async def init_db(app):
-    app['db'] = await db_engine
+# async def init_db(app):
+#     app['db'] = await db_engine
 
 
 async def close_db(app):
@@ -30,4 +33,4 @@ async def close_db(app):
 
 
 if __name__ == '__main__':
-    await create_tables(db_engine)
+    create_tables()
