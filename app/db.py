43c5d9eaae083763ff205880bed707489f40f7a4
witekosz <witekosz@gmail.com>
2019-11-07 08:39:47 +0100

    nie wiem zapytaj witka

--- app/db.py
+++ app/db.py
@@ -1,4 +1,5 @@
 import asyncio
+import logging
 from aiopg.sa import create_engine
 import sqlalchemy as sa
 
@@ -20,8 +21,29 @@ def create_tables(engine):
     meta.create_all(bind=engine, tables=[shelter, pet])
 
 
+def drop_tables(engine):
+    meta = sa.MetaData()
+    meta.drop_all(bind=engine, tables=[shelter, pet])
+
+
 async def init_db(app):
+    logging.info("DB Init")
     app['db'] = await db_engine
+    print(app['db'])
+
+
+def sample_data(engine):
+    conn = engine.connect()
+    conn.execute(shelter.insert(), [
+        {'question_text': 'What\'s new?',
+         'pub_date': '2015-12-15 17:17:49.629+02'}
+    ])
+    conn.execute(pet.insert(), [
+        {'choice_text': 'Not much', 'votes': 0, 'question_id': 1},
+        {'choice_text': 'The sky', 'votes': 0, 'question_id': 1},
+        {'choice_text': 'Just hacking again', 'votes': 0, 'question_id': 1},
+    ])
+    conn.close()
 
 
 async def close_db(app):
@@ -29,5 +51,6 @@ async def close_db(app):
     await app['db'].wait_closed()
 
 
-if __name__ == '__main__':
-    await create_tables(db_engine)
+# if __name__ == '__main__':
+    # create_tables(db_engine)
+    # sample_data(db_engine)
