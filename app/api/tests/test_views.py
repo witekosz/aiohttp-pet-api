870d2ed1c2449e98bbbc0bf4e2676c6e4537d5c5
witekosz <witekosz@gmail.com>
2019-11-11 21:44:24 +0100

    Refactor tests

--- app/api/tests/test_views.py
+++ app/api/tests/test_views.py
@@ -15,6 +15,7 @@ async def test_get_pets_view(aiohttp_client, test_app, loop):
     client = await aiohttp_client(test_app)
 
     resp = await client.get('/pets')
+    data = await resp.json()
 
     assert resp.status == 200
 
@@ -40,17 +41,17 @@ async def test_get_pets_detail_view_dummy_id(aiohttp_client, test_app, loop):
     assert "Invalid UUID format" in text
 
 
-async def test_delete_pets_view(aiohttp_client, test_app, loop):
+async def test_delete_pets_view(aiohttp_client, test_app, get_pet_id, loop):
     client = await aiohttp_client(test_app)
 
-    resp = await client.delete('/pets/sfdfadssfsda')
+    resp = await client.delete('/pets/' + get_pet_id)
     text = await resp.text()
 
-    assert resp.status == 400
-    assert "Invalid UUID format" in text
+    assert resp.status == 200
+    assert "deleted" in text
 
 
-async def test_get_shelter_detail_view_dummy_id(aiohttp_client, test_app, loop):
+async def test_get_shelter_detail_view(aiohttp_client, test_app, loop):
     client = await aiohttp_client(test_app)
 
     resp = await client.get('/shelters/lashfslhdsahsflafdslhsad')
@@ -67,19 +68,3 @@ async def test_delete_shelter_detail_view(aiohttp_client, test_app, loop):
     text = await resp.text()
 
     assert resp.status == 400
-
-
-async def test_correct_validate_uuid4_(loop):
-    uuid_string = '069cefc7-13d2-4dd2-9269-0df8c9d686c9'
-
-    validation = await validate_uuid4(uuid_string)
-
-    assert validation
-
-
-async def test_error_validate_uuid4_(loop):
-    uuid_string = '069cefc7-1-0dfsomeerrorstring9d686c9'
-
-    validation = await validate_uuid4(uuid_string)
-
-    assert not validation
