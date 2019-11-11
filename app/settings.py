afbd9ff20342b117ae50b38c177174907b0215b4
witekosz <witekosz@gmail.com>
2019-11-11 18:50:12 +0100

    Remove JSONEncoder

--- app/settings.py
+++ app/settings.py
@@ -6,7 +6,7 @@ BASE_DIR = pathlib.Path(__file__).parent.parent
 # DB settings
 POSTGRES_USER = 'api_user'
 POSTGRES_PASSWORD = 'psg_pass'
-POSTGRES_DB = 'shelter_api'
+POSTGRES_DB = 'pet_api'
 # POSTGRES_HOST = 'postgres'
 POSTGRES_HOST = 'localhost'
 POSTGRES_PORT = '5432'
